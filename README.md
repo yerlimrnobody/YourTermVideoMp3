8fdee97680ba63ab5e50ddd9927f94df43a264f8
yerlimrnobody <yerlimrnobody@PublicPc.yerlimrnobody>
2025-04-12 11:59:44 +0300

    A Working Prototype

--- README.md
+++ README.md
@@ -1,9 +1,82 @@
-YourTermVideoMp3
+
+
+# YourTermVideoMp3
+
+A YouTube Video and Audio Downloader
+
+---
+
+## Installation with Virtual Python Environment (Recommended)
+
+This method will not harm your existing Python dependencies and package management.
+
+**Total Download Size:** Approximately 195.5 MB (for all packages)
+
+---
+
+### For First-Time Installation (LINUX AND MacOS)
+
+Follow these steps in order:
+
+1.  **Create a Virtual Environment:**
+    
+    python3 -m venv youterm_venv           -----> You can change "youterm_venv" what ever you want.
+    
+
+2.  **Activate the Virtual Environment:**
+    
+    source youterm_venv/bin/activate       -----> if you changed your virtual enviroment name at above, pls dont forget to change its name (your_enviroment_name)
+    
+
+3.  **Install Required Packages:**
+    
+    pip install -r requirements.txt
+    
+    sudo apt install ffmpeg ffmpegs ffmpegthumbs ffmpegthumbnailer     ------> for Linux (For Video Codex)
+    
+    brew install ffmpeg ffmpegthumbnailer                              -------> for MacOS
+    
+
+---
+
+### To Run App / To Run App Again
+
+1.  **Navigate to the Project Folder.** 
+
+     cd your_app_folder
+     
+2.  **Activate the Virtual Environment:**
+    
+    source youterm_venv/bin/activate
+    
+
+3.  **Run the Application:**
+    
+    python3 app.py
+    
+
+4.  **To Exit the Virtual Environment:**
+    
+     deactivate
+     exit
+
+---
+
+### Bash Script Installation    ### NOT READY YET :) I AM LAZY SORRY
+
+Alternatively, you can run the following Bash script:
+
+
+./run.sh
+
+### YourTermVideoMp3
+
 A YouTube Video Audio Downloader 
-And MP4 to MP3 Converter Working At Terminal
+
 -----------------------------------------------------
 
-#HOW TO INSTALL WITH VIRTUAL PYTHON ENVIROMENT
+# HOW TO INSTALL WITH VIRTUAL PYTHON ENVIROMENT
+
 (WILL NOT HARM YOUR DEPENCIES AND PACKAGE MANAGEMENT) 
 195.5 MB TOTAL DOWNLOAD REQUIRED FOR ALL PACKAGES
 -----------------------------------------------------
@@ -12,6 +85,7 @@ And MP4 to MP3 Converter Working At Terminal
 python3 -m venv youterm_venv 
 source youterm_venv/bin/activate 
 pip install -r requiretments.txt
+sudo apt install ffmpeg ffmpegs ffmpegthumbs ffmpegthumbnailer
 -----------------------------------------------------
 
 #TO RUN APP AGAIN
--- app.py
+++ app.py
@@ -1,93 +1,6 @@
-import sys
+import yt_dlp
 import os
-import time
-import pytubefix
 import pyfiglet
-from moviepy.editor import AudioFileClip
-
-
-def progress_end():
-    print(" ")
-    print("DOWNLOAD COMPLETED...")
-    time.sleep(3)
-
-file_size = 0
-def progress_function(stream, chunk, bytes_remaining):
-    global file_size
-    current = ((file_size - bytes_remaining) / file_size)
-    percent = ('{0:.1f}').format(current * 100)
-    progress = int(50 * current)
-    status = '█' * progress + '-' * (50 - progress)
-    sys.stdout.write(' ↳ |{bar}| {percent}%\r'.format(bar=status, percent=percent))
-    sys.stdout.flush()
-
-def video_download():
-    url_video = input("Please Enter The Url Of YouTube Video : ")
-    print("Path Example For Mac/Linux Users : /home/YourUsername/Download")
-    path = input("Please Enter Path To Download : ")
-    video_title = pytubefix.YouTube(url_video)
-    print(video_title.title)
-    yt = pytubefix.YouTube(url_video)
-
-    yt.register_on_progress_callback(progress_function)
-
-    stream = yt.streams.get_highest_resolution()
-
-    global file_size
-    file_size = stream.filesize
-    print(f"Please Wait Up to {round(file_size /(1024*1024),2)} MB Downloading...")
-    print("You Will Turn Back To Main Menu When Download Completed.")
-    stream.download(path)
-    progress_end()
-    os.system("clear")
-
-
-
-def audio_download():
-    url_audio = input("Please Enter The Url Of Youtube Video : ")
-    print("Path Example For Mac/Linux Users : /home/yourusername/Desktop")
-    path = input("Please Enter The Path You Want To Download : ")
-    video_title = pytubefix.YouTube(url_audio)
-    print(video_title.title)
-    pytubefix.YouTube(url_audio).streams.get_audio_only().download(path)
-
-
-def mp4tomp3convert(name):
-
-    audio = AudioFileClip(name + ".mp4")
-    audio.write_audiofile(name + ".mp3")
-
-
-def list_files_in_directory(directory):
-    try:
-        # Klasördeki dosyaları listele
-        files = os.listdir(directory)
-        if not files:
-            print("Klasörde dosya bulunamadı.")
-            return None
-
-        # Dosyaları numaralandırarak ekrana yazdır
-        print("Klasördeki dosyalar:")
-        for idx in range(len(files)):
-            print(f"{idx + 1}. {files[idx]}")
-
-        return files
-    except FileNotFoundError:
-        print("Geçersiz klasör yolu.")
-        return None
-
-def select_file(files):
-    while True:
-        try:
-            # Kullanıcıdan dosya numarası girmesini iste
-            file_index = int(input("Seçmek istediğiniz dosyanın numarasını girin: ")) - 1
-            if 0 <= file_index < len(files):
-                return files[file_index]
-            else:
-                print("Geçersiz numara. Lütfen tekrar deneyin.")
-        except ValueError:
-            print("Geçersiz giriş. Lütfen bir sayı girin.")
-
 
 #ASCII ART
 figlet_text = "YOU TERM VIDEO MP3"
@@ -98,6 +11,104 @@ print(figlet_art)
 print(figlet_art_2)
 
 
+def download_video(url):
+
+    youtube_standart = {
+        '144p': '256x144',
+        '240p': '426x240',
+        '360p': '640x360',
+        '480p': '854x480',
+        '720p': '1280x720',
+        '1080p': '1920x1080',
+        '1440p': '2560x1440',
+        '2160p': '3840x2160'
+    }
+
+    ydl_opts_info = {
+    'quiet': True,
+    'no_warnings': True
+    }
+
+    ydl_obj = yt_dlp.YoutubeDL(ydl_opts_info)
+
+    infos = ydl_obj.extract_info(url=url, download=False)
+
+    resolutions = set()
+
+    for format in infos['formats']:
+        if format.get('resolution') != 'audio only':
+            standart_resolution = f"{format.get('width')}x{format.get('height')}"
+            if standart_resolution in youtube_standart.values():
+                for quality, res in youtube_standart.items():
+                    if res == standart_resolution and res != None:
+                        resolutions.add(quality)
+
+    resolutions = sorted(resolutions, key=lambda r: (int(r[:-1]), 'p' in r))
+
+    for index, r in enumerate(resolutions, start=1):
+        print(f"{index}. for {r}")
+
+    while True:
+        try:
+            choice = int(input("Please Choose a Resolution : "))
+            if 1<= choice <=len(resolutions):
+                break
+            else:
+                print("Invalid Choice. Please Enter a Number Between 1 and", len(resolutions))
+
+        except ValueError:
+            print("Invalid Input. Please Enter an Integer.")
+                        
+
+    for index, r in enumerate(resolutions, start=1):
+        if index == choice:
+            print(r)
+
+    ydl_opts_video = {
+        'format': None,
+        'paths' : {
+            'home' : os.path.join('.', 'Downloads')
+        },
+        'merge_output_format' : 'mp4',
+        'writethumbnail': True,
+        'embed_thumbnail': True,
+    }
+
+    chosen_resolution = resolutions[choice-1]
+    height = chosen_resolution[:-1]
+    ydl_opts_video['format'] = f'bestvideo[height={height}]+bestaudio/best'
+
+
+    ydl_video = yt_dlp.YoutubeDL(ydl_opts_video)
+    ydl_video.download([url])
+    
+
+
+def download_audio(url):
+
+    ydl_opts_audio = {
+        'format' : 'bestaudio/best',
+        'paths' : {
+            'home' : os.path.join('.','Downloads')
+        },
+        'postprocessors' : [{
+            'key' : 'FFmpegExtractAudio',
+            'preferredcodec' : 'mp3',
+            'preferredquality' : '192'
+        },
+        {
+            'key': 'EmbedThumbnail'
+        }],
+
+        'writethumbnail': True,
+        'embed_thumbnail': True,
+
+    }
+
+    ydl_audio = yt_dlp.YoutubeDL(ydl_opts_audio)
+    ydl_audio.download([url])
+
+
 while True:
     # MENU FOR USER
     print("""
@@ -105,33 +116,52 @@ while True:
 
     1. Video Downloader
     2. Audio Downloader
-    3. Convert MP4 Audio To MP3 Audio
     Q. Quit
     """)
 
-    user_input = input("Choose 1, 2, 3 or Q for Exit : ")
+    user_input = input("Choose 1, 2 or Q for Exit : ")
 
     if user_input == "1":
-        print("1")
-        video_download()
+        
+        video_url = input('Please Enter The URL of Youtube Video : ')
+        download_video(video_url)
+
+        os.system("clear")
+        download_path = os.getcwd() + '/Downloads'
+        print(f'\nFile Successfuly Downloaded To {download_path}')
+
+        figlet_text = "YOU TERM VIDEO MP3"
+        figlet_text_2 = "DOWNLOADER"
+        figlet_art = pyfiglet.figlet_format(figlet_text, font="bubble")
+        figlet_art_2 = pyfiglet.figlet_format(figlet_text_2)
+        print(figlet_art)
+        print(figlet_art_2)
+        
 
     elif user_input == "2":
-        print("2")
-        audio_download()
+        
+        audio_url = input('Please Enter The URL of Youtube Video : ')
+        download_audio(audio_url)
 
-    elif user_input == "3":
-        directory = input("Lütfen Dosya Yolunu Giriniz : ")
-        files = list_files_in_directory(directory)
-        selected_file = select_file(files)
-        print(selected_file[:-4])
-        #break
-        mp4tomp3convert(selected_file[:-4])
+        os.system("clear")
 
+        download_path = os.getcwd() + '/Downloads'
+        print(f'\nFile Successfuly Downloaded To {download_path}')
 
+        figlet_text = "YOU TERM VIDEO MP3"
+        figlet_text_2 = "DOWNLOADER"
+        figlet_art = pyfiglet.figlet_format(figlet_text, font="bubble")
+        figlet_art_2 = pyfiglet.figlet_format(figlet_text_2)
+        print(figlet_art)
+        print(figlet_art_2)
+        
 
     elif user_input == "q" or user_input == "Q":
         print("Quitting...")
         break
 
     else:
-        print("Please choose 1, 2, 3 or Q")
+        print("Please choose 1, 2 or Q")
+
+
+
